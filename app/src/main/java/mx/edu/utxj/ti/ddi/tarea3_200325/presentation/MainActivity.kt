414887266c02b5d5e2e98b16a77600f051ce0317
Fabiola-BJ <fabiola.bautistajoaquin@utxicotepec.edumnx>
2023-07-21 13:02:28 -0600

    tarea 3

---
+++ app/src/main/java/mx/edu/utxj/ti/ddi/tarea3_200325/presentation/MainActivity.kt
@@ -0,0 +1,87 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+package mx.edu.utxj.ti.ddi.tarea3_200325.presentation
+
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import mx.edu.utxj.ti.ddi.tarea3_200561.R
+import mx.edu.utxj.ti.ddi.tarea3_200561.presentation.theme.Practica4_181193Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.setOnClickListener {
+
+
+
+
+            val colorAnim: ObjectAnimator = ObjectAnimator.ofInt(
+                textView, "textColor",
+                android.graphics.Color.BLUE, android.graphics.Color.RED
+            )
+            colorAnim.setEvaluator(ArgbEvaluator())
+            colorAnim.duration=1200
+            colorAnim.start()
+
+        }
+
+    }
+
+}
+
+@Composable
+fun WearApp(greetingName: String) {
+    Practica4_181193Theme {
+        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
+         * version of LazyColumn for wear devices with some added features. For more information,
+         * see d.android.com/wear/compose.
+         */
+        Column(
+                modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background),
+                verticalArrangement = Arrangement.Center
+        ) {
+            Greeting(greetingName = greetingName)
+        }
+    }
+}
+
+@Composable
+fun Greeting(greetingName: String) {
+    Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colors.primary,
+            text = stringResource(R.string.hello_world, greetingName)
+    )
+}
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    WearApp("Preview Android")
+}
